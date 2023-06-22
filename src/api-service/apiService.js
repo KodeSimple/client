2e2f3c9a33708cfa0ffb02dcb17a53ccfc580a42
Joel Siroy <kodesimple57@gmail.com>
2023-06-22 13:22:56 +0800

    add base url https://server-alpha-blue.vercel.app

--- src/api-service/apiService.js
+++ src/api-service/apiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiService = axios.create({
-       baseURL: '136.158.61.247:8000',
+    baseURL: 'https://server-alpha-blue.vercel.app', 
     // baseURL: 'http://localhost:8000',
 });
 
